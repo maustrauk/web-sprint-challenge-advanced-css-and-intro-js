558d42282e5cce33daeadb8adfcde7ea0cd01287
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-09 11:10:40 -0400

    js task 3

--- index.js
+++ index.js
@@ -227,8 +227,8 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(userArray, userIndex) {
+  return `The artist at index ${userIndex} is ${userArray[userIndex].name}.`
 }
 
   
